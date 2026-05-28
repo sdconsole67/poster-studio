c2afb89c88c1c0d3c745cd7224b1a9f764504128
swarup das <gobardhanpur713143@gmail.com>
2026-05-29 01:02:40 +0530

    new verson with dipsik

--- js/index.js
+++
@@ -1,7 +0,0 @@
-function startEditor(type) {
-  localStorage.setItem("editorStartType", type);
-  localStorage.removeItem("selectedDesign");
-  localStorage.removeItem("editorDraft");
-
-  window.location.href = "editor.html";
-}
